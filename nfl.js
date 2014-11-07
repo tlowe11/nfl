28c4be43ed5097a025acd1e5c27928cad80e032d
Taylor <taylorlowe7@gmail.com>
2014-11-06 21:19:04 -0800

    first commit

---
+++ nfl.js
@@ -0,0 +1,13 @@
+$(function (){ 
+var footballTeams = [
+
+    {nfcEast: [{Giants:[{w:34}, {l:20}]}, {Redskins:[{w:34}, {l:20}]}, {Eagles:[{w:34}, {l:20}]}, {Cowboys:[{w:34}, {l:20}]}
+
+]}
+];
+
+console.log(footballTeams[0].nfcEast[1].Redskins[1].l);
+
+
+
+});
